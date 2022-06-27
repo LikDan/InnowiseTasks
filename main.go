2a5464836b6e80fd19ab7722a38e16bcbd56d427
LikDan <likdan.company@gmail.com>
2022-06-27 14:40:59 +0300

    Create fourth task

--- main.go
+++ main.go
@@ -1,5 +1,41 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math"
+	"os"
+)
 
+func toInt(str string) (i int, err error) {
+	for index, b := range str {
+		if int(b)-48 < 0 || int(b)-48 > 9 {
+			return 0, fmt.Errorf("not a number")
+		}
+
+		if len(str) == index+1 {
+			i += int(b) - 48
+			break
+		}
+		i += (int(b) - 48) * int(math.Pow(10, float64(len(str)-index-1)))
+	}
+
+	return
+}
+
+func task4() {
+	if len(os.Args) != 2 {
+		fmt.Println("Enter a number")
+		return
+	}
+
+	i, err := toInt(os.Args[1])
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	fmt.Println(i)
+}
+
+func main() {
+	task4()
 }
