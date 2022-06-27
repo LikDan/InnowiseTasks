6b99086fbaed42cf8c7702676b2b6d7a135586c5
LikDan <likdan.company@gmail.com>
2022-06-27 16:39:46 +0300

    Resolved

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"time"
 )
 
 func fibonacci(num int) int {
@@ -18,6 +19,30 @@ func fibonacci(num int) int {
 	return fibonacci(num-1) + fibonacci(num-2)
 }
 
+func fibonacciLoop(num int) int {
+	if num < 1 {
+		return 0
+	}
+
+	if num == 2 {
+		return 1
+	}
+
+	n1 := 1
+	n2 := 0
+
+	for i := 1; ; i++ {
+		if i == num {
+			return n1
+		}
+
+		n := n1 + n2
+
+		n2 = n1
+		n1 = n
+	}
+}
+
 func task5() {
 	if len(os.Args) != 2 {
 		fmt.Println("Enter a number")
@@ -30,8 +55,19 @@ func task5() {
 		return
 	}
 
-	f := fibonacci(num)
-	fmt.Println(f)
+	t := time.Now().UnixMicro()
+	for i := 0; i < num; i++ {
+		fmt.Print(fibonacci(i+1), "\t")
+	}
+	t = time.Now().UnixMicro() - t
+	fmt.Println("Recursion: " + strconv.FormatInt(t, 10))
+
+	t = time.Now().UnixMicro()
+	for i := 0; i < num; i++ {
+		fmt.Print(fibonacciLoop(i+1), "\t")
+	}
+	t = time.Now().UnixMicro() - t
+	fmt.Println("Loop: " + strconv.FormatInt(t, 10))
 }
 
 func main() {
