75a792fbb0e76f4fbb534e3a69a4d7fdf588163d
LikDan <likdan.company@gmail.com>
2022-06-27 14:50:18 +0300

    Create fifth task

--- main.go
+++ main.go
@@ -1,5 +1,39 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
 
+func fibonacci(num int) int {
+	if num < 0 {
+		return 0
+	}
+
+	if num == 1 || num == 2 {
+		return 1
+	}
+
+	return fibonacci(num-1) + fibonacci(num-2)
+}
+
+func task5() {
+	if len(os.Args) != 2 {
+		fmt.Println("Enter a number")
+		return
+	}
+
+	num, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Println("Enter a valid number")
+		return
+	}
+
+	f := fibonacci(num)
+	fmt.Println(f)
+}
+
+func main() {
+	task5()
 }
